e17fb08311066a7d5b7ab949ccb513eceee49a52
ats527 <ats527@github.com>
2024-04-18 20:17:56 +0530

    small fix

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -16,7 +16,7 @@ const sendToken = (user, statusCode, res) => {
         httpOnly: false,
         withCredentials: true,
         sameSite: "none",
-        secure: true,
+        secure: false,
     };
 
     res.cookie('token', token, options);
